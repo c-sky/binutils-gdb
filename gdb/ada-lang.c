bdb224d009e9633face6f22b4f8b6889a40a43d2
Pedro Alves <palves@redhat.com>
2017-06-01 17:03:19 +0100

    Make the linespec/location completer ignore data symbols
    Currently "b foo[TAB]" offers data symbols as completion candidates.
    This doesn't make sense, since you can't set a breakpoint on data
    symbols, only on code symbols.
    
     (gdb) b globa[TAB]
     (gdb) b global [ENTER]
     Function "global" not defined.
     Make breakpoint pending on future shared library load? (y or [n]) n
     (gdb) info symbol global
     global in section .rodata
    
    So this patch makes linespec completion ignore data symbols.
    
    gdb/ChangeLog:
    yyyy-mm-dd  Pedro Alves  <palves@redhat.com>
    
    	* ada-lang.c (ada_make_symbol_completion_list): Use
    	completion_skip_symbol.
    	* symtab.c (symbol_is_function_or_method(minimal_symbol*)): New.
    	(symbol_is_function_or_method(symbol*)): New.
    	(add_symtab_completions): Add complete_symbol_mode parameter.  Use
    	completion_skip_symbol.
    	(default_collect_symbol_completion_matches_break_on): Use
    	completion_skip_symbol.  Pass down mode.
    	(collect_file_symbol_completion_matches): Pass down mode.
    	* symtab.h (symbol_is_function_or_method): New declarations.
    	(completion_skip_symbol): New template function.

--- gdb/ada-lang.c
+++ gdb/ada-lang.c
@@ -6463,6 +6463,9 @@ ada_collect_symbol_completion_matches (completion_tracker &tracker,
   {
     QUIT;
 
+    if (completion_skip_symbol (mode, msymbol))
+      continue;
+
     completion_list_add_name (tracker,
 			      MSYMBOL_LANGUAGE (msymbol),
 			      MSYMBOL_LINKAGE_NAME (msymbol),
@@ -6479,6 +6482,9 @@ ada_collect_symbol_completion_matches (completion_tracker &tracker,
 
       ALL_BLOCK_SYMBOLS (b, iter, sym)
       {
+	if (completion_skip_symbol (mode, sym))
+	  continue;
+
 	completion_list_add_name (tracker,
 				  SYMBOL_LANGUAGE (sym),
 				  SYMBOL_LINKAGE_NAME (sym),
@@ -6495,6 +6501,9 @@ ada_collect_symbol_completion_matches (completion_tracker &tracker,
     b = BLOCKVECTOR_BLOCK (COMPUNIT_BLOCKVECTOR (s), GLOBAL_BLOCK);
     ALL_BLOCK_SYMBOLS (b, iter, sym)
     {
+      if (completion_skip_symbol (mode, sym))
+	continue;
+
       completion_list_add_name (tracker,
 				SYMBOL_LANGUAGE (sym),
 				SYMBOL_LINKAGE_NAME (sym),
@@ -6511,6 +6520,9 @@ ada_collect_symbol_completion_matches (completion_tracker &tracker,
       continue;
     ALL_BLOCK_SYMBOLS (b, iter, sym)
     {
+      if (completion_skip_symbol (mode, sym))
+	continue;
+
       completion_list_add_name (tracker,
 				SYMBOL_LANGUAGE (sym),
 				SYMBOL_LINKAGE_NAME (sym),
